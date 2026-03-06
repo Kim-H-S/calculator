415421500d35676d6a38de58350a2a0c94d5a397
KHS <kij4300@naver.com>
2026-03-06 14:06:05 +0900

    HTML 계산기UI
    HTML로 계산기 UI를 만들었다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 export default function App() {
   return (
     <>
-      <>App</>
+      <article className="calculator">
+        <div className="buttons">
+          <output>아웃풋</output>
+          <button className="clear">C</button>
+          <button className="operator">*</button>
+          <button className="number">7</button>
+          <button className="number">8</button>
+          <button className="number">9</button>
+          <button className="operator">/</button>
+          <button className="number">4</button>
+          <button className="number">5</button>
+          <button className="number">6</button>
+          <button className="operator">-</button>
+          <button className="number">1</button>
+          <button className="number">2</button>
+          <button className="number">3</button>
+          <button className="operator">+</button>
+          <button className="number">0</button>
+          <button className="result">=</button>
+        </div>
+      </article>
     </>
   );
 }
