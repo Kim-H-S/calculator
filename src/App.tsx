92d90dca57628ebeb0f8957714a5cf242f56823a
KHS <kij4300@naver.com>
2026-03-07 12:52:15 +0900

    버튼 클릭
    버튼 클릭 시 값이 로그로 출력한다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,86 @@
+import { useState } from "react";
+
 export default function App() {
+  const [state, setState] = useState({
+    previousNumber: "",
+    currentNumber: "0",
+    isNewNumber: true,
+    operation: null,
+  });
+
+  const handleClear = () => {
+    console.log("clear");
+  };
+
+  const handleOperatorClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    console.log(event.currentTarget.textContent);
+  };
+
+  const handleNumberClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    console.log(event.currentTarget.textContent);
+  };
+
   return (
     <>
       <article className="calculator">
         <div className="buttons">
-          <output>아웃풋</output>
-          <button className="clear">C</button>
+          <output>{state.currentNumber}</output>
+          <button className="clear" onClick={handleClear}>
+            C
+          </button>
           <div></div>
           <div></div>
-          <button className="operator">/</button>
-          <button className="number">7</button>
-          <button className="number">8</button>
-          <button className="number">9</button>
-          <button className="operator">*</button>
-          <button className="number">4</button>
-          <button className="number">5</button>
-          <button className="number">6</button>
-          <button className="operator">-</button>
-          <button className="number">1</button>
-          <button className="number">2</button>
-          <button className="number">3</button>
-          <button className="operator">+</button>
+          <button className="operator" onClick={handleOperatorClick}>
+            /
+          </button>
+          <button className="number" onClick={handleNumberClick}>
+            7
+          </button>
+          <button className="number" onClick={handleNumberClick}>
+            8
+          </button>
+          <button className="number" onClick={handleNumberClick}>
+            9
+          </button>
+          <button className="operator" onClick={handleOperatorClick}>
+            *
+          </button>
+          <button className="number" onClick={handleNumberClick}>
+            4
+          </button>
+          <button className="number" onClick={handleNumberClick}>
+            5
+          </button>
+          <button className="number" onClick={handleNumberClick}>
+            6
+          </button>
+          <button className="operator" onClick={handleOperatorClick}>
+            -
+          </button>
+          <button className="number" onClick={handleNumberClick}>
+            1
+          </button>
+          <button className="number" onClick={handleNumberClick}>
+            2
+          </button>
+          <button className="number" onClick={handleNumberClick}>
+            3
+          </button>
+          <button className="operator" onClick={handleOperatorClick}>
+            +
+          </button>
           <div></div>
-          <button className="number">0</button>
+          <button className="number" onClick={handleOperatorClick}>
+            0
+          </button>
           <div></div>
-          <button className="result">=</button>
+          <button className="result" onClick={handleOperatorClick}>
+            =
+          </button>
         </div>
       </article>
     </>
