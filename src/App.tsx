323d8fbc056cfb7f446e6cf744bd1bbda85d9c99
KHS <kij4300@naver.com>
2026-03-07 12:04:45 +0900

    버튼 순서
    *, / 버튼 순서 바꿨다.

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ export default function App() {
         <div className="buttons">
           <output>아웃풋</output>
           <button className="clear">C</button>
-          <button className="operator">*</button>
+          <button className="operator">/</button>
           <button className="number">7</button>
           <button className="number">8</button>
           <button className="number">9</button>
-          <button className="operator">/</button>
+          <button className="operator">*</button>
           <button className="number">4</button>
           <button className="number">5</button>
           <button className="number">6</button>
