b0669f51dfc4c49ea261e634016312a7d2a5b980
KHS <kij4300@naver.com>
2026-03-08 19:24:07 +0900

    숫자 입력
    숫자 버튼을 누르면 숫자를 입력할 수 있다.

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,22 @@ export default function App() {
   const handleNumberClick = (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
   ) => {
-    console.log(event.currentTarget.textContent);
+    // // 테스트 코드
+    // console.log(event.currentTarget.textContent);
+
+    const value = event.currentTarget.value;
+    if (state.isNewNumber) {
+      setState({
+        ...state,
+        currentNumber: value,
+        isNewNumber: false,
+      });
+    } else {
+      setState({
+        ...state,
+        currentNumber: state.currentNumber + value,
+      });
+    }
   };
 
   return (
@@ -37,44 +52,44 @@ export default function App() {
           <button className="operator" onClick={handleOperatorClick}>
             /
           </button>
-          <button className="number" onClick={handleNumberClick}>
+          <button className="number" value="7" onClick={handleNumberClick}>
             7
           </button>
-          <button className="number" onClick={handleNumberClick}>
+          <button className="number" value="8" onClick={handleNumberClick}>
             8
           </button>
-          <button className="number" onClick={handleNumberClick}>
+          <button className="number" value="9" onClick={handleNumberClick}>
             9
           </button>
           <button className="operator" onClick={handleOperatorClick}>
             *
           </button>
-          <button className="number" onClick={handleNumberClick}>
+          <button className="number" value="4" onClick={handleNumberClick}>
             4
           </button>
-          <button className="number" onClick={handleNumberClick}>
+          <button className="number" value="5" onClick={handleNumberClick}>
             5
           </button>
-          <button className="number" onClick={handleNumberClick}>
+          <button className="number" value="6" onClick={handleNumberClick}>
             6
           </button>
           <button className="operator" onClick={handleOperatorClick}>
             -
           </button>
-          <button className="number" onClick={handleNumberClick}>
+          <button className="number" value="1" onClick={handleNumberClick}>
             1
           </button>
-          <button className="number" onClick={handleNumberClick}>
+          <button className="number" value="2" onClick={handleNumberClick}>
             2
           </button>
-          <button className="number" onClick={handleNumberClick}>
+          <button className="number" value="3" onClick={handleNumberClick}>
             3
           </button>
           <button className="operator" onClick={handleOperatorClick}>
             +
           </button>
           <div></div>
-          <button className="number" onClick={handleOperatorClick}>
+          <button className="number" value="0" onClick={handleNumberClick}>
             0
           </button>
           <div></div>
